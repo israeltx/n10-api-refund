1f6d1bed056cc872d20a43414cb3a16a9c6e78a3
Israel Teixeira <israeltx@gmail.com>
2025-08-06 13:51:57 -0300

    Validate new users

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express"
+import { UserRole } from "@prisma/client"
+import { z } from "zod"
 
 class UsersController {
  async create(request: Request, response: Response) {
-  response.json({ message:'Create controller ok' })
+  const bodySchema = z.object({
+    name: z.string().trim().min(2, {message: 'O nome é obrigatório'}),
+    email: z.string().trim().email({message: 'Email inválido'}).toLowerCase(),
+    password: z.string().min(6, {message: 'Mínimo de 6 caracteres'}),
+    role: z.enum([UserRole.employee, UserRole.manager]).default(UserRole.employee)
+  })
+
+  const {name, email, password, role} = bodySchema.parse(request.body)
+
+  response.json({ name, email, password, role })
  }
 
 }
