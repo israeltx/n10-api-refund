118c9c269050c7c4731a643d8e1c9b62b34a9da8
Israel Teixeira <israeltx@gmail.com>
2025-08-06 13:01:33 -0300

    Intall express-async-errors and Zod; Start error handling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express from "express";
 import cors from "cors"
+import { errorHandling } from "./middlewares/error-handling";
+import { AppError } from "./utils/AppError";
+import { z } from "zod"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => {
+  const bodySchema = z.object({
+    age: z.number().min(18)
+  })
 
+  const { age } = bodySchema.parse(req.body)
+
+  res.send('Hello World')
+})
+
+app.use(errorHandling)
 export { app }
