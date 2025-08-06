d86494c3a5defa100e7929380a4b73262e6eb120
Israel Teixeira <israeltx@gmail.com>
2025-08-06 14:24:27 -0300

    Recover user

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -2,6 +2,7 @@ import { AppError } from "@/utils/AppError"
 import { Request, Response } from "express"
 import { prisma } from "@/database/prisma"
 import { UserRole } from "@prisma/client"
+import { compare } from "bcrypt"
 import { z } from "zod"
 
 class SessionsController {
@@ -12,6 +13,18 @@ class SessionsController {
   })
 
   const { email, password } = bodySchema.parse(request.body)
+
+  const user = await prisma.user.findFirst({where:{email}})
+
+  if (!user) {
+    throw new AppError('Email ou senha inválidos', 401)
+  }
+
+  const passwordMatched = await compare(password, user.password)
+
+  if (!passwordMatched) {
+    throw new AppError('Email ou senha inválidos', 401)
+  }
 
   response.status(201).json({email, password})
  }
