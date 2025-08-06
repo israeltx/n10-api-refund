f92cff7f90d862b77decfadefe7c36b5c822e5d4
Israel Teixeira <israeltx@gmail.com>
2025-08-06 14:20:43 -0300

    Validate sessions

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -6,14 +6,14 @@ import { z } from "zod"
 
 class SessionsController {
  async create(request: Request, response: Response) {
-  // const bodySchema = z.object({
-  //   name: z.string().trim().min(2, {message: 'O nome é obrigatório'}),
-  //   email: z.string().trim().email({message: 'Email inválido'}).toLowerCase(),
-  //   password: z.string().min(6, {message: 'Mínimo de 6 caracteres'}),
-  //   role: z.enum([UserRole.employee, UserRole.manager]).default(UserRole.employee)
-  // })
+  const bodySchema = z.object({
+    email: z.string().trim().email({message: 'Email inválido'}).toLowerCase(),
+    password: z.string(),
+  })
 
-  response.status(201).json({message: 'Session controller ok'})
+  const { email, password } = bodySchema.parse(request.body)
+
+  response.status(201).json({email, password})
  }
 
 }
