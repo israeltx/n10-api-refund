4fc3f2d0e18a6aea2c09bc58cabbb13f8fe48d6b
Israel Teixeira <israeltx@gmail.com>
2025-08-06 15:28:48 -0300

    Ensure user authorization

--- src/routes/refunds-routes.ts
+++ src/routes/refunds-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { RefundsController } from "@/controllers/refunds-controller";
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
 
 const refundsRoutes = Router()
 const sessionsController = new RefundsController()
 
-refundsRoutes.post('/', sessionsController.create)
+refundsRoutes.post('/', verifyUserAuthorization(['manager']), sessionsController.create)
 
 export { refundsRoutes }
