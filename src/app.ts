5fb8412f874da26f8d877cefb10abd15d1bd9fa4
Israel Teixeira <israeltx@gmail.com>
2025-08-06 13:06:08 -0300

    Install and set prisma

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,10 @@
 import express from "express";
 import cors from "cors"
 import { errorHandling } from "./middlewares/error-handling";
-import { AppError } from "./utils/AppError";
-import { z } from "zod"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
-
-app.get('/', (req, res) => {
-  const bodySchema = z.object({
-    age: z.number().min(18)
-  })
-
-  const { age } = bodySchema.parse(req.body)
-
-  res.send('Hello World')
-})
 
 app.use(errorHandling)
 export { app }
