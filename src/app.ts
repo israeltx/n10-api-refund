337430e53ee9cedc1fe2afdb7100212356fc603c
Israel Teixeira <israeltx@gmail.com>
2025-08-07 13:54:46 -0300

    Create route to exibit file

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-import "express-async-errors"
-import express from "express";
-import cors from "cors"
 import { errorHandling } from "./middlewares/error-handling";
 import { routes } from "./routes";
+import "express-async-errors";
+import express from "express";
+import cors from "cors"
+import uploadConfig from "@/configs/upload"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+app.use('/uploads', express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(routes)
 app.use(errorHandling)
