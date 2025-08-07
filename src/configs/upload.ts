983c6c8cc4a04511525965bbdd8247b3a2c2ff59
Israel Teixeira <israeltx@gmail.com>
2025-08-07 13:21:00 -0300

    Validate upload file

--- src/configs/upload.ts
+++ src/configs/upload.ts
@@ -5,7 +5,8 @@ import crypto from "node:crypto"
 const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp')
 const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, 'uploads')
 
-const MAX_FILE_SIZE = 1024 * 1024 * 3 //3 Mb
+const MAX_SIZE = 3 //3 Mb
+const MAX_FILE_SIZE = 1024 * 1024 * MAX_SIZE
 const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg','image/png']
 
 const MULTER = {
@@ -23,6 +24,7 @@ const MULTER = {
 export default {
   TMP_FOLDER,
   UPLOADS_FOLDER,
+  MAX_SIZE,
   MULTER,
   MAX_FILE_SIZE,
   ACCEPTED_IMAGE_TYPES
--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,8 +1,29 @@
 import { Request, Response } from "express";
+import uploadConfig from "@/configs/upload"
+import z from "zod";
 
 class UploadsController {
   async create(request: Request, response: Response) {
-    response.json({message:'Uploads controller ok'})
+    try {
+      const fileSchema = z.object({
+        filename: z.string().min(1, 'Arquivo é obrigatório'),
+        mimetype: z.string().refine((type) => {
+          uploadConfig.ACCEPTED_IMAGE_TYPES.includes(type),
+          `Formato de arquivo inválido. Formatos permitidos: ${uploadConfig.ACCEPTED_IMAGE_TYPES}`
+        }),
+        size: z.number().positive().refine((size) => {
+          size <= uploadConfig.MAX_FILE_SIZE,
+          `O arquivo excede ao tamanho máximo de ${uploadConfig.MAX_FILE_SIZE}`
+        })
+      }).passthrough()
+
+      const { file } = fileSchema.parse(request.file)
+
+      response.json({message: 'Validação da imagem ok'})
+
+    } catch (error) {
+      throw error
+    }
   }
 }
 
